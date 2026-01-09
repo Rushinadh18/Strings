04133b511a59cab8f02db7d920114cc9d85b3583
Siva Rushinadh <23a31a42b0@Pragati.ac.in>
2026-01-09 19:47:37 +0530

    Add files via upload

---
+++ rot.java
@@ -0,0 +1,10 @@
+public class rot {
+    public static void main(String[] args) {
+        
+    
+        String d="abcd";
+        String b="cdab";
+        System.out.println(d.length()==b.length());
+    }
+    
+}
