c0c2a05f3ca29de03d2b628dbc6496b48b492843
rushi <karanamrushi834@gmail.com>
2026-01-13 22:01:22 +0530

    this ios a main

---
+++ object.java
@@ -0,0 +1,19 @@
+public class object {
+    int Main;
+    int ami;
+int hi(int pami,int pMain){
+    return ami+Main;
+}
+
+    static void main(String[] args){
+        object n=new object();
+        n.Main=29;
+        n.ami=10;
+      System.out.println(  n.hi(n.ami,n.Main));
+
+
+        
+
+    }
+    
+}
