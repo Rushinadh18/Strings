76de665d855eda82ea93e56166992de88c070391
rushi <karanamrushi834@gmail.com>
2026-01-24 12:59:09 +0530

    this is a msg

---
+++ hey.java
@@ -0,0 +1,14 @@
+public class hey {
+    int a=90,b=2;
+   final int c=9;
+    static void main(String... args){
+        hey h=new hey();
+        h.a=99;
+        h.b=4;
+        
+        System.out.println(h.a);
+        System.out.println(h.b);
+        System.out.println(h.c);
+    }
+    
+}
--- later.java
+++ later.java
@@ -1,5 +1,5 @@
 class later{
-    static int  jim(){
+    int  jim(){
         
     }
     static void main(String[] args){
