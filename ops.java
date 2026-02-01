2dc530bb3ef2faae4472ea402baf007c4768fc5c
rushi <karanamrushi834@gmail.com>
2026-02-01 17:15:03 +0530

    this is a nonminate

---
+++ ops.java
@@ -0,0 +1,17 @@
+public class ops {
+
+    private int v = 90;
+
+ int get() {
+        return v;
+    }
+    static void min(){
+        System.out.println(10*2);
+    }
+
+    public static void main(String[] args) {
+        ops hi = new ops();
+        System.out.println(hi.get());
+        min();
+    }
+}
