cd8aeb6d7afc84f5535525a669e05f8d024111b7
rushi <karanamrushi834@gmail.com>
2026-02-03 04:07:14 +0530

    at 4'00clock

---
+++ add12.java
@@ -0,0 +1,13 @@
+class add{
+int a =2;
+int b=3;
+void add1(){
+System.out.println(a+b);
+}
+}
+class additon extends add{
+static void main(String[] args){
+additon hi=new additon();
+hi.add1();
+}
+}
