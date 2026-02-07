5772e5430fd5e879094ae63eebcbae2f814cc556
rushi <karanamrushi834@gmail.com>
2026-02-07 12:06:36 +0530

    this is a msg

---
+++ inheit.java
@@ -0,0 +1,13 @@
+class fcuk {
+String a="the is a msg and a commit";
+void vom(){
+    System.out.println(a);
+}
+}
+public class inheit extends fcuk{
+public static void main(String... args){
+        inheit hi=new inheit();
+        hi.vom();
+    }
+}
+
