9e17df4a5b22fdfa3e96831929059a76bfb57a37
rushi <karanamrushi834@gmail.com>
2026-02-17 18:52:46 +0530

    this is too lag

---
+++ inheritb.java
@@ -0,0 +1,13 @@
+ class bonm{
+    int jio(){
+        return 2;
+    }
+}
+
+ class inheritb extends bonm  {
+static void main(String[] args){
+    inheritb hi=new inheritb();
+    int h=hi.jio();
+    System.out.println(h*2);
+}    
+}
